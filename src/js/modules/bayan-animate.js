9a777a5e520c8208e7ce7c7ddf621b04e661c5c3
dstepas <dstepas@justcoded.co>
2019-02-22 19:17:03 +0200

    merge changes

--- src/js/modules/bayan-animate.js
+++ src/js/modules/bayan-animate.js
@@ -1,46 +1,157 @@
 /* eslint-disable*/
 
+// const animate = ($ => {
+
+//   const $section = $('.four-slide');
+
+//   if (!$section) return;
+
+//   const $holder = $section.find('.slide');
+//   const $frame1 = $section.find('.frame1');
+//   const $frame2 = $section.find('.frame2');
+//   const $frame3 = $section.find('.frame3');
+//   const $frame4 = $section.find('.frame4');
+
+//   const ACTIVE = 'active';
+//   const OPENER = 'opener';
+//   const SHOW = 'show';
+
+//   const init = () => {
+//     _bayan();
+//   };
+
+//   let _bayan = () => {
+//     $holder.hover(function (e) {
+//       $holder.removeClass(ACTIVE).removeClass(OPENER);
+//       $(this).addClass(ACTIVE);
+//       _imgTransform($(this));
+//     });
+//   };
+
+//   let _imgTransform = (th) => {
+//     if (th.find($frame1).length !== 0) {
+//       $frame2.addClass(SHOW);
+//     } else {
+//       $frame2.removeClass(SHOW);
+//     }
+//     if (th.find($frame4).length !== 0) {
+//       $frame3.addClass(SHOW);
+//     } else {
+//       $frame3.removeClass(SHOW);
+//     }
+//   }
+
+//   return {
+//     init
+//   };
+
+// })(jQuery);
+
+// export default animate;
+
 const animate = ($ => {
 
-  const $section = $('.four-slide');
+  const $holder = $('.slide');
+  const ACTIVE_CLASS = 'active';
+  const CURRENT_CLASS = 'current';
+  const INNER_HOLDER_CLASS = 'inner-holder';
 
-  const $picture = $section.find('.slide-heading');
-  const $holder = $section.find('.slide');
-  const $active = $section.find('.active.slide');
-  const $frame1 = $section.find('.frame1');
-  const $frame2 = $section.find('.frame2');
-  const $frame3 = $section.find('.frame3');
-  const $frame4 = $section.find('.frame4');
-  
-  const ACTIVE = 'active';
-  const OPENER = 'opener';
-  const SHOW = 'show';
+  if (!($holder)) return;
 
-  if (!($picture && $holder && $active && $frame1 && $frame2 && $frame3 && $frame4)) return;
+  let $checker;
+
+  $(window).on('resize', 'load', () => {
+    let $windowWidth = $(window).innerWidth();
+    if ($windowWidth < 768) {
+      $checker = true;
+      console.log($checker);
+    } else {
+      $checker = false;
+      console.log($checker);
+    };
+    console.log($windowWidth);
+  });
+
+  // let _checker = () => {
+  //   if ($(window).width() <= 767) {
+  //     return;
+  //   };
+  // }
+
+  // _checker();
+
+  // let waitForFinalEvent = (() => {
+  //   let timers = {};
+  //   return (callback, ms, uniqueId) => {
+  //     if (timers[uniqueId]) {
+  //       clearTimeout(timers[uniqueId]);
+  //     }
+  //     timers[uniqueId] = setTimeout(callback, ms);
+  //   };
+  // })();
+
+  // $(window).resize(() => {
+  //   waitForFinalEvent(function () {
+  //     _checker();
+  //   }, 500);
+  // });
 
   const init = () => {
-    _bayan();
-  };
 
-  let _bayan = () => {
-    $holder.hover(function (e) {
-      $holder.removeClass(ACTIVE).removeClass(OPENER);
-      $(this).addClass(ACTIVE);
-      _imgTransform($(this));
-    });
-  };
+    // if ($checker) return;
 
-  let _imgTransform = (th) => {
-    if (th.find($frame1).length !== 0) {
-      $frame2.addClass(SHOW);
-    } else {
-      $frame2.removeClass(SHOW);
-    }
-    if (th.find($frame4).length !== 0) {
-      $frame3.addClass(SHOW);
-    } else {
-      $frame3.removeClass(SHOW);
-    }
+    const positionsPool = [
+      [0, 50, 100, 150],
+      [50, 50, 100, 150],
+      [50, 100, 100, 150],
+      [50, 100, 150, 150]
+    ];
+    const slidesQuantity = positionsPool.length;
+
+    let slideActiveindex = $holder.index($(`.${CURRENT_CLASS}`));
+    let StartPositions = positionsPool[slideActiveindex];
+
+    let $preLastInnerHolder = $($holder[slidesQuantity - 2]).find(`.${INNER_HOLDER_CLASS}`);
+
+    $holder.each(function (i, element) {
+
+      let translateValue = StartPositions[i];
+
+      $(element).css({
+        'transform': `translateX(-${translateValue}%)`
+      })
+
+      $(element).on('mouseenter', function () {
+
+        let prevActiveIndex = $holder.index($(`.${CURRENT_CLASS}`));
+        let currentItemStartPosition = StartPositions[i];
+        let prevItemStartPosition = StartPositions[prevActiveIndex];
+        let ActiveElementIndex = i;
+
+        console.log(prevActiveIndex);
+
+        $($holder[prevActiveIndex]).removeClass(CURRENT_CLASS).removeClass(ACTIVE_CLASS);
+        $(this).addClass(CURRENT_CLASS).addClass(ACTIVE_CLASS);
+
+        $holder.each(function (i, element) {
+          let currentPosition = positionsPool[ActiveElementIndex][i];
+          $(element).css({
+            'transform': `translateX(-${currentPosition}%)`
+          })
+        });
+
+        $preLastInnerHolder.css({
+          'transform': 'translateX(0)'
+        });
+
+        if (i == slidesQuantity - 1) {
+          $preLastInnerHolder.css({
+            'transform': 'translateX(50%)'
+          })
+        }
+
+      })
+    })
   }
 
   return {
--- src/js/modules/mobile-gallery.js
+++ src/js/modules/mobile-gallery.js
@@ -1,35 +1,36 @@
 /* eslint-disable*/
 
-const gallery = ($ => {
-
+const gallery = (($) => {
   const $section = $('.four-slide');
+
+  if (!($section)) return;
 
   const ACTIVE = 'active';
   const OPENER = 'opener';
 
-  const HOLDER = 'solutionss';
+  const HOLDER = 'solutions';
   const SLIDE = 'slide';
-  const WRAPPER = 'accordionn';
+  const WRAPPER = 'accordion';
+
+  const widthBreakpoint = 768;
 
   const SW_HOLDER = 'swiper-container';
   const SW_SLIDE = 'swiper-slide';
   const SW_WRAPPER = 'swiper-wrapper';
 
-  const breakpoint = window.matchMedia('(min-width:768px)');
+  const breakpoint = window.matchMedia(`(min-width:${widthBreakpoint}px)`);
 
   let mySwiper;
 
-  if (!($section)) return;
-
   const _replaceClasses = (holder, wrapper, slide, chHolder, chWrapper, chSlide) => {
-    const $holder = $section.find('.' + holder);
-    const $slide = $section.find('.' + slide);
-    const $wrapper = $section.find('.' + wrapper);
+    const $holder = $section.find(`.${holder}`);
+    const $slide = $section.find(`.${slide}`);
+    const $wrapper = $section.find(`.${wrapper}`);
     $holder.removeClass().addClass(chHolder);
     $slide.removeClass().addClass(chSlide);
     $wrapper.removeClass().addClass(chWrapper);
-    if (!($slide.eq(2).hasClass(SW_SLIDE))) $slide.eq(2).addClass(OPENER);
-    console.log($section);
+    $slide.removeAttr('style');
+    if (!($slide.eq(2).hasClass(SW_SLIDE))) $slide.eq(2).addClass('current');
   };
 
   const _destroySwiper = () => {
@@ -46,27 +47,25 @@ const gallery = ($ => {
 
   const _startAnimation = () => {
     const $slide = $section.find('.swiper-slide');
-    const $slide_acitve = $section.find('.swiper-slide-active');
+    const $slideAcitve = $section.find('.swiper-slide-active');
     $slide.removeClass(ACTIVE);
-    $slide_acitve.addClass(ACTIVE);
+    $slideAcitve.addClass(ACTIVE);
   };
 
   const _breakpointChecker = () => {
-
     if (breakpoint.matches === true) {
       if (mySwiper !== undefined) _destroySwiper();
       return _removeSwiper();
-    } else if (breakpoint.matches === false) {
+    } if (breakpoint.matches === false) {
       return _addSwiper(), _enableSwiper();
-    };
+    }
   };
 
   const _enableSwiper = () => {
-    mySwiper = new Swiper($section.find('.' + SW_HOLDER), {
+    mySwiper = new Swiper($section.find(`.${SW_HOLDER}`), {
       slidesPerView: 1,
       direction: 'vertical',
       loop: true,
-      autoHeight: true,
       pagination: {
         el: '.swiper-pagination',
         type: 'bullets',
@@ -74,8 +73,8 @@ const gallery = ($ => {
       },
     });
     mySwiper.on('slideChangeTransitionEnd', () => {
-      _startAnimation()
-    })
+      _startAnimation();
+    });
   };
 
   breakpoint.addListener(_breakpointChecker);
@@ -85,9 +84,8 @@ const gallery = ($ => {
   };
 
   return {
-    init
+    init,
   };
-
 })(jQuery);
 
 export default gallery;
