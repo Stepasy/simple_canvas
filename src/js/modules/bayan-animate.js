f0af6330b6f60aa16c87e441356e439ee2affc67
dstepas <dstepas@justcoded.co>
2019-02-22 19:51:01 +0200

    destroy

--- src/js/modules/bayan-animate.js
+++ src/js/modules/bayan-animate.js
@@ -49,115 +49,174 @@
 
 // export default animate;
 
-const animate = ($ => {
+// const animate = ($ => {
 
+//   const $holder = $('.slide');
+//   const ACTIVE_CLASS = 'active';
+//   const CURRENT_CLASS = 'current';
+//   const INNER_HOLDER_CLASS = 'inner-holder';
+
+//   if (!($holder)) return;
+
+//   const init = () => {
+
+//     const positionsPool = [
+//       [0, 50, 100, 150],
+//       [50, 50, 100, 150],
+//       [50, 100, 100, 150],
+//       [50, 100, 150, 150]
+//     ];
+//     const slidesQuantity = positionsPool.length;
+
+//     let slideActiveindex = $holder.index($(`.${CURRENT_CLASS}`));
+//     let StartPositions = positionsPool[slideActiveindex];
+
+//     let $preLastInnerHolder = $($holder[slidesQuantity - 2]).find(`.${INNER_HOLDER_CLASS}`);
+
+//     $holder.each(function (i, element) {
+
+//       let translateValue = StartPositions[i];
+
+//       $(element).css({
+//         'transform': `translateX(-${translateValue}%)`
+//       })
+
+//       $(element).on('mouseenter', function () {
+
+//         let prevActiveIndex = $holder.index($(`.${CURRENT_CLASS}`));
+//         let currentItemStartPosition = StartPositions[i];
+//         let prevItemStartPosition = StartPositions[prevActiveIndex];
+//         let ActiveElementIndex = i;
+
+//         console.log(prevActiveIndex);
+
+//         $($holder[prevActiveIndex]).removeClass(CURRENT_CLASS).removeClass(ACTIVE_CLASS);
+//         $(this).addClass(CURRENT_CLASS).addClass(ACTIVE_CLASS);
+
+//         $holder.each(function (i, element) {
+//           let currentPosition = positionsPool[ActiveElementIndex][i];
+//           $(element).css({
+//             'transform': `translateX(-${currentPosition}%)`
+//           })
+//         });
+
+//         $preLastInnerHolder.css({
+//           'transform': 'translateX(0)'
+//         });
+
+//         if (i == slidesQuantity - 1) {
+//           $preLastInnerHolder.css({
+//             'transform': 'translateX(50%)'
+//           })
+//         }
+
+//       })
+//     })
+//   }
+
+//   return {
+//     init
+//   };
+
+// })(jQuery);
+
+// export default animate;
+
+const ANIMATE = ($ => {
   const $holder = $('.slide');
   const ACTIVE_CLASS = 'active';
   const CURRENT_CLASS = 'current';
   const INNER_HOLDER_CLASS = 'inner-holder';
-
+  const BREAKPOINT = 768;
   if (!($holder)) return;
 
-  let $checker;
-
-  $(window).on('resize', 'load', () => {
-    let $windowWidth = $(window).innerWidth();
-    if ($windowWidth < 768) {
-      $checker = true;
-      console.log($checker);
-    } else {
-      $checker = false;
-      console.log($checker);
+  var waitForFinalEvent = (function () {
+    var timers = {};
+    return function (callback, ms, uniqueId) {
+      if (timers[uniqueId]) {
+        clearTimeout(timers[uniqueId]);
+      }
+      timers[uniqueId] = setTimeout(callback, ms);
     };
-    console.log($windowWidth);
-  });
+  })();
 
-  // let _checker = () => {
-  //   if ($(window).width() <= 767) {
-  //     return;
-  //   };
-  // }
+  function accordionSlider() {
 
-  // _checker();
+    let isMobileHeader;
 
-  // let waitForFinalEvent = (() => {
-  //   let timers = {};
-  //   return (callback, ms, uniqueId) => {
-  //     if (timers[uniqueId]) {
-  //       clearTimeout(timers[uniqueId]);
-  //     }
-  //     timers[uniqueId] = setTimeout(callback, ms);
-  //   };
-  // })();
+    $(window).on('resize load', function () {
+      waitForFinalEvent(function () {
+        isMobileHeader = $(window).width() < BREAKPOINT;
+        horizontalSlider();
+      }, 500);
+    });
 
-  // $(window).resize(() => {
-  //   waitForFinalEvent(function () {
-  //     _checker();
-  //   }, 500);
-  // });
+    console.log(isMobileHeader);
+
+    function horizontalSlider() {
+
+      if (isMobileHeader) {
+        console.log('123');
+        _destroy();
+        return false;
+      };
+
+      const positionsPool = [
+        [0, 50, 100, 150],
+        [50, 50, 100, 150],
+        [50, 100, 100, 150],
+        [50, 100, 150, 150]
+      ];
+      const slidesQuantity = positionsPool.length;
+      let slideActiveindex = $holder.index($(`.${CURRENT_CLASS}`));
+      let StartPositions = positionsPool[slideActiveindex];
+      let $preLastInnerHolder = $($holder[slidesQuantity - 2]).find(`.${INNER_HOLDER_CLASS}`);
+      $holder.each(function (i, element) {
+        let translateValue = StartPositions[i];
+        $(element).css({
+          'transform': `translateX(-${translateValue}%)`
+        })
+        $(element).on('mouseenter', function () {
+          let prevActiveIndex = $holder.index($(`.${CURRENT_CLASS}`));
+          let currentItemStartPosition = StartPositions[i];
+          let prevItemStartPosition = StartPositions[prevActiveIndex];
+          let ActiveElementIndex = i;
+          $($holder[prevActiveIndex]).removeClass(CURRENT_CLASS).removeClass(ACTIVE_CLASS);
+          $(this).addClass(CURRENT_CLASS).addClass(ACTIVE_CLASS);
+          $holder.each(function (i, element) {
+            let currentPosition = positionsPool[ActiveElementIndex][i];
+            $(element).css({
+              'transform': `translateX(-${currentPosition}%)`
+            })
+          });
+          $preLastInnerHolder.css({
+            'transform': 'translateX(0)'
+          });
+          if (i == slidesQuantity - 1) {
+            $preLastInnerHolder.css({
+              'transform': 'translateX(50%)'
+            })
+          }
+        })
+      })
+
+      function _destroy() {
+        $holder.each(function (i, element) {
+          $(element).css({
+            'transform': `translateX(0)`
+          })
+          $(element).unbind('mouseenter');
+        })
+      }
+    }
+  }
 
   const init = () => {
-
-    // if ($checker) return;
-
-    const positionsPool = [
-      [0, 50, 100, 150],
-      [50, 50, 100, 150],
-      [50, 100, 100, 150],
-      [50, 100, 150, 150]
-    ];
-    const slidesQuantity = positionsPool.length;
-
-    let slideActiveindex = $holder.index($(`.${CURRENT_CLASS}`));
-    let StartPositions = positionsPool[slideActiveindex];
-
-    let $preLastInnerHolder = $($holder[slidesQuantity - 2]).find(`.${INNER_HOLDER_CLASS}`);
-
-    $holder.each(function (i, element) {
-
-      let translateValue = StartPositions[i];
-
-      $(element).css({
-        'transform': `translateX(-${translateValue}%)`
-      })
-
-      $(element).on('mouseenter', function () {
-
-        let prevActiveIndex = $holder.index($(`.${CURRENT_CLASS}`));
-        let currentItemStartPosition = StartPositions[i];
-        let prevItemStartPosition = StartPositions[prevActiveIndex];
-        let ActiveElementIndex = i;
-
-        console.log(prevActiveIndex);
-
-        $($holder[prevActiveIndex]).removeClass(CURRENT_CLASS).removeClass(ACTIVE_CLASS);
-        $(this).addClass(CURRENT_CLASS).addClass(ACTIVE_CLASS);
-
-        $holder.each(function (i, element) {
-          let currentPosition = positionsPool[ActiveElementIndex][i];
-          $(element).css({
-            'transform': `translateX(-${currentPosition}%)`
-          })
-        });
-
-        $preLastInnerHolder.css({
-          'transform': 'translateX(0)'
-        });
-
-        if (i == slidesQuantity - 1) {
-          $preLastInnerHolder.css({
-            'transform': 'translateX(50%)'
-          })
-        }
-
-      })
-    })
+    accordionSlider();
   }
 
   return {
     init
   };
-
 })(jQuery);
-
-export default animate;
+export default ANIMATE;
