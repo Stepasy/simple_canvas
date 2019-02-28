5fad5d86ee36f7ffe94556aae1ade75a6e8da8e8
dstepas <dstepas@justcoded.co>
2019-02-28 12:33:11 +0200

    animations delay

--- src/js/modules/boxes.js
+++ src/js/modules/boxes.js
@@ -9,34 +9,59 @@ const boxes = ($ => {
   const _animation = () => {
     const $boxes = $('.boxes');
     if ($boxes.length === 0) return;
+
     const BOX = 'box';
     const ACTIVE = 'active';
     const HOVERED = 'hovered';
-    const $box = $boxes.find(`.${BOX}`);
-    const $delay = 1000;
-    let $timer;
 
-    const _timer = (el, time) => {
-      $timer = setTimeout(() => {
-        $(el).addClass(ACTIVE).removeClass(HOVERED);
+    const $box = $boxes.find(`.${BOX}`);
+
+    const $delay = 1000;
+    let $addingTimer;
+    let $removingTimer;
+
+    const _classAdding = (el) => {
+      $(el).addClass(ACTIVE).removeClass(HOVERED);
+    }
+
+    const _addingTimer = (el, time) => {
+      $addingTimer = setTimeout(() => {
+        _classAdding(el);
       }, time);
     };
 
-    const _stopTimer = () => {
-      clearTimeout($timer);
+    const _stopAddingTimer = () => {
+      clearTimeout($addingTimer);
+    };
+
+    const _classRemoving = (el) => {
+      $(el).removeClass(ACTIVE);
+      $box.removeClass(ACTIVE);
+    }
+
+    const _removingTimer = (el, time) => {
+      $removingTimer = setTimeout(() => {
+        _classRemoving(el);
+      }, time);
+    };
+
+    const _stopRemovingTimer = () => {
+      clearTimeout($removingTimer);
     };
 
     const _addClass = (el, event) => {
       $(el).on(event, () => {
-        $(el).addClass(HOVERED);
-        _timer(el, $delay)
+        if (!($(el).hasClass(ACTIVE))) $(el).addClass(HOVERED);
+        _addingTimer(el, $delay);
       });
     };
 
     const _removeClass = (el, event) => {
       $(el).on(event, () => {
-        _stopTimer();
-        $(el).removeClass(ACTIVE).removeClass(HOVERED);
+        _stopAddingTimer();
+        _stopRemovingTimer();
+        _removingTimer(el, $delay);
+        $(el).removeClass(HOVERED);
       });
     };
 
